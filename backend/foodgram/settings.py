4419075291d41a4f86edc010fea7fd8d1cfdba28
Igor Ravlis <seiju.pioneer23@gmail.com>
2023-10-21 01:08:03 +0000

    deploy with static

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -141,10 +141,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = '/backend_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/backend_media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
